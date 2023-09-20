611f8d58bb9b71c2bdb7563f2a6d490c0dc4204d
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-09-20 20:44:11 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "91a779c8-7f7c-422f-bb5b-1d3869619060",
+    "home_feed": [
+        {
+            "id": "f1366bcb-6af8-4721-87c7-51c32d2b74a3",
+            "artist_name": "cold",
+            "home_carousel": [
+                {
+                    "id": "189c65bb-c72f-4acd-8738-23f3d75a0923",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/212.png"
+                },
+                {
+                    "id": "22076a89-423e-406e-82b6-522e5b7d6b83",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png"
+                },
+                {
+                    "id": "925a930c-9b10-4286-bdfb-10680b25ec07",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/27.png"
+                },
+                {
+                    "id": "0c343f8f-b0dc-4d0c-a7d0-57ae85278c8b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/296.png"
+                },
+                {
+                    "id": "02bc6cb8-5f09-4055-9d3f-e512d580be5b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/160.png"
+                }
+            ],
+            "art_caption": "time",
+            "art_description": "Right tree fire early drug theory. Response parent section type kind. Yourself develop example about fact."
+        },
+        {
+            "id": "4459dbe1-15f7-4902-9bde-dce12c7eb081",
+            "artist_name": "course",
+            "home_carousel": [
+                {
+                    "id": "f0f347d7-4bb0-408e-9595-f7dc7f6dc121",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/145.png"
+                },
+                {
+                    "id": "0c0189ff-b76d-4d18-9fc4-d0d48ec62ae6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/363.png"
+                },
+                {
+                    "id": "75825c52-3523-4c47-b6a7-c22962aec182",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/188.png"
+                },
+                {
+                    "id": "0cadbc14-0628-4052-9370-030465323346",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/252.png"
+                },
+                {
+                    "id": "3e2dafbc-544a-47ac-ac9b-d377f3e7b170",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/118.png"
+                }
+            ],
+            "art_caption": "benefit",
+            "art_description": "Page throughout company question back line. Benefit story keep former base whom each."
+        },
+        {
+            "id": "63c62fb6-9015-4a56-8744-1258fae8d30a",
+            "artist_name": "walk",
+            "home_carousel": [
+                {
+                    "id": "9bddc7b8-8299-4c6c-9053-0edf4fc214c8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/7.png"
+                },
+                {
+                    "id": "65b70f6e-bfd5-4e95-8998-41cb1edaa759",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/220.png"
+                },
+                {
+                    "id": "5582cb03-89ac-4ff6-bcaf-7110be855b6e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png"
+                },
+                {
+                    "id": "0936e835-798a-4757-8364-f48678ef1871",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/327.png"
+                },
+                {
+                    "id": "bd51b5d8-27ed-41a1-92fa-574a2fc88da4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/67.png"
+                }
+            ],
+            "art_caption": "wait",
+            "art_description": "Certain future near ago pick force majority. Simply rather Mrs goal talk suddenly very. Store institution at film citizen town add."
+        },
+        {
+            "id": "452e53ee-68a6-458b-a5ef-080d7b8735ec",
+            "artist_name": "each",
+            "home_carousel": [
+                {
+                    "id": "9198657c-7527-4cb1-9fb5-75ebc2b7c3f3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/357.png"
+                },
+                {
+                    "id": "af9bdaa5-3397-46bc-8c8f-a995d324a481",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/337.png"
+                },
+                {
+                    "id": "1d5bfe6f-27d8-41fa-9822-8a514a783482",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/250.png"
+                },
+                {
+                    "id": "936c179f-38cf-4930-becb-5e8f13980e2b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/345.png"
+                },
+                {
+                    "id": "86537038-2a8e-4009-a196-6619ee2e1020",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/223.png"
+                }
+            ],
+            "art_caption": "health",
+            "art_description": "Position blue policy step on product himself. Face something produce cell five decide. Together so some edge yard respond."
+        },
+        {
+            "id": "896110da-2ce1-4de0-b9bf-464e0fe2da68",
+            "artist_name": "true",
+            "home_carousel": [
+                {
+                    "id": "040a773e-1ebc-45ed-9175-4da08ce431ae",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/168.png"
+                },
+                {
+                    "id": "b016e105-e414-4c02-a866-b610f108542f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/66.png"
+                },
+                {
+                    "id": "10f0ab35-48f2-41f1-ba30-7af76e4edf59",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/214.png"
+                },
+                {
+                    "id": "772301ac-4632-4957-ad2e-b5789f170113",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/175.png"
+                },
+                {
+                    "id": "5a9d7e7a-e957-406d-846f-4b36f1f8092d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/115.png"
+                }
+            ],
+            "art_caption": "former",
+            "art_description": "His court exactly. Painting yard recent finish. Street help always material always social."
+        }
+    ]
+}
